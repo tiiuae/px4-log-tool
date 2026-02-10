70d0a986021963f750038d79ea77a5a45d342e7a
Junior Sundar <junior.sundar@tii.ae>
2026-02-10 10:56:40 +0400

    bump: version to 0.6.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name="px4-log-tool",
-    version="0.6.3",
+    version="0.6.4",
     description="The All-in-One tool to work with PX4 log files.",
     author="Junior Sundar",
     author_email="junior.sundar@tii.ae",
